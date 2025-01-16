ae767584939cb765b426feb82f90842057e21792
Marco Antonio Russo <ocra94@gmail.com>
2025-01-16 23:59:30 +0100

    gpt 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     staticPageGenerationTimeout: 240,
     trailingSlash: true,
-    assetPrefix: process.env.NODE_ENV === "production" ? "/kikoffy" : "",
-    basePath: process.env.NODE_ENV === "production" ? "/kikoffy" : "",
+    assetPrefix: "/kikoffy",
+    basePath: "/kikoffy",
 };
