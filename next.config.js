07f26152e476cadd4d2dc6b955100fa312e6e71d
Marco Antonio Russo <ocra94@gmail.com>
2025-01-16 23:27:23 +0100

    deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 module.exports = {
     staticPageGenerationTimeout: 240,
+    exportTrailingSlash: true,
+    assetPrefix: process.env.NODE_ENV === "production" ? "/kikoffy" : "",
+    basePath: process.env.NODE_ENV === "production" ? "/kikoffy" : "",
 };
-const nextConfig = {
-    output: "export", // <=== enables static exports
-    images: {
-        unoptimized: true,
-    },
-};
-
-module.exports = nextConfig;
