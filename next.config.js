3e51728c3fb26f7e94ef2106a094ca4869573931
Marco Antonio Russo <ocra94@gmail.com>
2025-01-16 22:35:08 +0100

    ,

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  staticPageGenerationTimeout: 240,
+    staticPageGenerationTimeout: 240,
 };
+const nextConfig = {
+    basePath: "/kikoffy",
+    output: "export", // <=== enables static exports
+    reactStrictMode: true,
+};
+
+module.exports = nextConfig;
