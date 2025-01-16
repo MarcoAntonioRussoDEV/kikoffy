917943a784f2eabd876993b7cf10662358652734
Marco Antonio Russo <ocra94@gmail.com>
2025-01-16 23:53:51 +0100

    chatGPT

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
-    output: "export",
+    staticPageGenerationTimeout: 240,
+    trailingSlash: true,
+    assetPrefix: process.env.NODE_ENV === "production" ? "/kikoffy" : "",
+    basePath: process.env.NODE_ENV === "production" ? "/kikoffy" : "",
 };
